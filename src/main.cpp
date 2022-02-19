96004fea3cd444fd62d5a99408e077f4b3653f93
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2022-02-19 15:36:09 +0500

    testing websockets

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "firebase/firebase.h"
+// #include "firebase/firebase.h"
 #include "fingerprint/fingerprint.h"
 #if defined(ESP32)
 #include <WiFi.h>
@@ -10,6 +10,8 @@
 #define WIFI_PASSWORD "WIFI_PASSWORD"
 
 unsigned long sendDataPrevMillis = 0;
+
+WiFiServer wifiServer(80);
 
 void setup()
 {
@@ -26,36 +28,62 @@ void setup()
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
 
-  firebaseInit();
+  wifiServer.begin();
+  // firebaseInit();
   FingerprintInit();
 }
 
 void loop()
 {
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
+  WiFiClient client = wifiServer.available();
+  String command = "";
+  if (client)
   {
-    sendDataPrevMillis = millis();
+    while (client.connected())
+    {
+      while (client.available() > 0)
+      {
+        char c = client.read();
 
-    Serial.print("Set int async... ");
-    auth.token.uid.c_str();
-        // for (size_t i = 0; i < 10; i++)
-        // {
-
-        // The purpose for this async function is to set, push and update data instantly but...
-
-        //The async function is implemented by the library and not supported by Firebase.
-
-        // When use this async function, the response will be ignored, then sometimes server may reject the subsequence requests or
-        // full of receive buffer can be interrupted the async continuous operation.
-
-        // You do not need to use these async functions in general usage.
-
-        // Use library in Test Mode (see examples/Authentication/TestMode) or legacy database secret authentication,
-        // the speed of data transfer will be increased.
-
-        // Firebase.RTDB.setIntAsync(&fbdo, "/test/int", count);
-        // count++;
-        // }
-        Serial.println("ok");
+        if (c == '\n')
+        {
+          break;
+        }
+        
+        command += c;
+        Serial.write(c);
+      }
+      delay(10);
+      command = "";
+    }
+    client.stop();
+    Serial.println('Client disconnected');
   }
+
+  // if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
+  // {
+  //   sendDataPrevMillis = millis();
+
+  //   Serial.print("Set int async... ");
+  //   auth.token.uid.c_str();
+  // for (size_t i = 0; i < 10; i++)
+  // {
+
+  // The purpose for this async function is to set, push and update data instantly but...
+
+  // The async function is implemented by the library and not supported by Firebase.
+
+  // When use this async function, the response will be ignored, then sometimes server may reject the subsequence requests or
+  // full of receive buffer can be interrupted the async continuous operation.
+
+  // You do not need to use these async functions in general usage.
+
+  // Use library in Test Mode (see examples/Authentication/TestMode) or legacy database secret authentication,
+  // the speed of data transfer will be increased.
+
+  // Firebase.RTDB.setIntAsync(&fbdo, "/test/int", count);
+  // count++;
+  // }
+  //       Serial.println("ok");
+  // }
 }
