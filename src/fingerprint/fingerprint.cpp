ab93dc3411838ff76c4ce7d49d207c2248e879ab
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2022-02-19 13:12:01 +0500

    delete function

--- src/fingerprint/fingerprint.cpp
+++ src/fingerprint/fingerprint.cpp
@@ -319,3 +319,23 @@ uint8_t getFingerprintEnroll(uint8_t id)
 
     return true;
 }
+
+uint8_t deleteFingerprint(uint8_t id) {
+  uint8_t p = -1;
+
+  p = finger.deleteModel(id);
+
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Deleted!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+  } else if (p == FINGERPRINT_BADLOCATION) {
+    Serial.println("Could not delete in that location");
+  } else if (p == FINGERPRINT_FLASHERR) {
+    Serial.println("Error writing to flash");
+  } else {
+    Serial.print("Unknown error: 0x"); Serial.println(p, HEX);
+  }
+
+  return p;
+}
