1915beb3c5291d373423cf9a91f9c082eca9d58a
Hussain Saifuddin <hussainquetta53@gmail.com>
2022-02-23 20:01:25 +0500

    websocket

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,108 @@
-#if defined(ESP32)
-#include <WiFi.h>
-#elif defined(ESP8266)
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-websocket-server-arduino/
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+// Import required libraries
 #include <ESP8266WiFi.h>
-#endif
-// #include "firebase/firebase.h"
-#include "fingerprint/fingerprint.h"
-#include "websocket/websocket.h"
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
 
-#define WIFI_SSID "ZONG MBB-E5573-79A8"
-#define WIFI_PASSWORD "43825940"
+// Replace with your network credentials
+const char* ssid = "QUETTAWALA";
+const char* password = "nouseyour3g";
 
-unsigned long sendDataPrevMillis = 0;
+DynamicJsonDocument doc(1024);
 
-
-
+// Set your Static IP address
 IPAddress local_IP(192, 168, 1, 184);
+// Set your Gateway IP address
 IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 0, 0);
 
-void setup()
-{
+IPAddress subnet(255, 255, 0, 0);
+IPAddress primaryDNS(8, 8, 8, 8);   //optional
+IPAddress secondaryDNS(8, 8, 4, 4); //optiona
+const int analogInPin = A0;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+void commandInstructions(char* data) {
+  ws.textAll(String(data));
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    commandInstructions((char*)data);
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+    switch (type) {
+      case WS_EVT_CONNECT:
+        Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+        break;
+      case WS_EVT_DISCONNECT:
+        Serial.printf("WebSocket client #%u disconnected\n", client->id());
+        break;
+      case WS_EVT_DATA:
+        handleWebSocketMessage(arg, data, len);
+        break;
+      case WS_EVT_PONG:
+      case WS_EVT_ERROR:
+        break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+void notifyClients() {
+    const uint8_t size = JSON_OBJECT_SIZE(1);
+    StaticJsonDocument<size> json;
+    json["battery"] = analogRead(analogInPin);
+
+    char data[17];
+    size_t len = serializeJson(json, data);
+    ws.textAll(data, len);
+}
+
+void setup() {
+  // Serial port for debugging purposes
   Serial.begin(115200);
 
-  if (!WiFi.config(local_IP,gateway,subnet)) {
-    Serial.println("STA Failed to configure");
+  // if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+  //   Serial.println("STA Failed to configure");
+  // }
+  // Connect to Wi-Fi
+  pinMode(14, INPUT);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
   }
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(300);
-  }
-  Serial.println();
-  Serial.print("Connected with IP: ");
+  // Print ESP Local IP Address
   Serial.println(WiFi.localIP());
 
-  pinMode(16, OUTPUT); //#D0 Finger lock
-
   initWebSocket();
+
+  // Start server
   server.begin();
 }
 
-void loop()
-{
+void loop() {
   ws.cleanupClients();
-  // ws.textAll();
-  // if ( getFingerprintID() == 1)
-  // {
-  //   Serial.println("Congrats you are the owner");
-  //   digitalWrite(16, HIGH);
-  // }
+  notifyClients();
+  delay(1000);
 }
